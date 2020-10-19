c0a68c744dce838e1ac6a6506e033fa6514d0720
Shrey Jain <jainshreyp@gmail.com>
2020-10-19 19:43:43 -0400

    added f string

--- fbstats.py
+++ fbstats.py
@@ -6,6 +6,6 @@ import pandas as pd
 data = pd.DataFrame()
 
 for i in range(2000,2020):
-    df = pd.read_html('https://www.pro-football-reference.com/years/2019/penalties.htm')[0].set_index('Penalty')['Pen/G']
+    df = pd.read_html(f'https://www.pro-football-reference.com/years/{i}/penalties.htm')[0].set_index('Penalty')['Pen/G']
     data[str(i)] = df
 data.to_csv('data.csv')
